68dbcdc47ffd42058f124170072ca793151371c2
João Pedro Medeiros <jpmdrsdev@gmail.com>
2024-07-10 17:41:17 -0300

    feat: using method post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express' //importando express
 const app = express() //cirar umas instancia express 
 
+app.use(express.json()) //indicar para o express ler body com json
+
 //mock ou esconder dados
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo: 'G'},
@@ -11,11 +13,16 @@ const selecoes = [
 
 // criar rota padrão ou raiz
 app.get('/', (req, res) => {
-    res.send('OOla mundo000!!!!!')
+    res.send('Selecoes Copa 22!!!!!')
 })
 
 app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
+})
+
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com Sucesso!')
 })
 
 export default app
