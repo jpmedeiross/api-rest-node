ff4e0abb829477a0a67d2f03ff7001ac0537618f
João Pedro Medeiros <jpmdrsdev@gmail.com>
2024-07-18 19:38:34 -0300

    feat: create and connecting mysql db

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import app from "./src/app.js"
+
+import conexao from './infra/conexao.js'
+
 const PORT = 3000 //definir a PORTa
 
-//escutar a porta 3000
+// fazer a conexao
+conexao.connect((error) =>{
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('Conexão realizada com sucesso!')
+        //escutar a porta 3000
 app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
+    }
+})
+
--- src/app.js
+++ src/app.js
@@ -11,6 +11,16 @@ const selecoes = [
     {id:4, selecao: 'Camarões', grupo: 'G'},
 ]
 
+//funcao p/ buscar o id
+function buscarSelecaoPorId(id) {
+    return selecoes.filter( selecao => selecao.id == id)
+}
+
+//funcao p/ pegar o index
+function buscaIndexSelecao(id) {
+    return selecoes.findIndex( selecao => selecao.id == id )
+}
+
 // criar rota padrão ou raiz
 app.get('/', (req, res) => {
     res.send('Selecoes Copa 22!!!!!')
@@ -20,9 +30,26 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com Sucesso!')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send('Seleção excluida com sucesso!')
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
